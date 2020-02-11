7feeb566ccac1f4b0f169c16ced051965a3cd131
Taj <taj.nijjar2002@gmail.com>
2020-02-11 14:24:47 +1300

    Added dummy data.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Todos from './components/Todos';
 
 function App() {
-  return <div>Abc</div>;
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Attend restro meeting',
+      done: false
+    },
+    {
+      id: 2,
+      title: 'Attend the amazing React class',
+      done: false
+    },
+    {
+      id: 3,
+      title: 'Feed yourself',
+      done: false
+    }
+  ]);
+
+  return (
+    <React.Fragment>
+      <Todos />
+    </React.Fragment>
+  );
 }
 
 export default App;
